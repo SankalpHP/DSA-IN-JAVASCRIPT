ef449f4dfe1631c13c1cfc054bd4c21292362334
Sankalp <selokarsankalp@gmail.com>
2025-12-25 16:39:11 +0530

    pop-&-print

--- 07-Linked-list/02-DoublyLinkedList/01-DoublyLinkedList.js
+++ 07-Linked-list/02-DoublyLinkedList/01-DoublyLinkedList.js
@@ -31,16 +31,40 @@ class DoublyLinkedList{
         this.length++;
         return this;
     }
+
+    pop(){
+        if(!this.head) return undefined
+        let popNode = this.tail;
+        if(this.length === 1){
+            this.head = null;
+            this.tail = null;
+        }else{
+            this.tail = popNode.prev;
+            this.tail.next  = null;
+            popNode.prev = null;
+        }
+        this.length--;
+        return popNode;
+    }
+
+    print(){
+        let current = list.head;
+        while(current){
+            console.log("List Item => ", current);
+            current = current.next;
+        }
+    }
 }
 
 let list = new DoublyLinkedList();
 list.push(1);
 list.push(2);
 list.push(3);
-// console.log(list);
 
-let current = list.head;
-while(current){
-    console.log(current);
-    current = current.next;
-}
+list.print();
+
+console.log("pop => ", list.pop());
+
+list.print();
+
+
