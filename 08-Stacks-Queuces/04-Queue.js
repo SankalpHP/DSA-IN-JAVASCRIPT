8e19932c33a15f9e2ac9d8e83d9efcc6e4868fc5
Sankalp <selokarsankalp@gmail.com>
2026-01-02 16:20:46 +0530

    Queue using linkedlist

--- 08-Stacks-Queuces/04-Queue.js
+++ 08-Stacks-Queuces/04-Queue.js
@@ -1,39 +1,84 @@
 /**
- *   Create a queue using linkedlist
+ * QUEUE IMPLEMENTATION USING SINGLY LINKED LIST
+ * 
+ * FIFO – First In, First Out
+ * Enqueue & Dequeue operations run in O(1) time
  */
 
-class Node{
-    constructor(val){
-        this.val = val,
-        this.next = null
+// Node represents a single element in the queue
+class Node {
+    constructor(val) {
+        this.val = val;      // Value stored in the node
+        this.next = null;    // Pointer to the next node
     }
 }
 
-class Queue{
-    constructor(){
-        this.first = null,
-        this.last = null,
-        this.size = 0
+// Queue class using a linked list
+class Queue {
+    constructor() {
+        this.first = null;   // Front of the queue
+        this.last = null;    // Rear of the queue
+        this.size = 0;       // Number of elements in the queue
     }
 
-    enqueue(val){
-
+    /**
+     * enqueue(val)
+     * Adds a value to the end of the queue
+     * Time Complexity: O(1)
+     */
+    enqueue(val) {
         let newNode = new Node(val);
 
-        if(!this.first) {
+        // If queue is empty
+        if (!this.first) {
             this.first = newNode;
             this.last = newNode;
-        }else{
+        } 
+        // If queue has elements
+        else {
             this.last.next = newNode;
             this.last = newNode;
         }
+
         return ++this.size;
+    }
+
+    /**
+     * dequeue()
+     * Removes a value from the front of the queue
+     * Time Complexity: O(1)
+     */
+    dequeue() {
+        if (this.size === 0) return null;
+
+        let removeNode = this.first;
+
+        // If only one element exists
+        if (this.size === 1) {
+            this.first = null;
+            this.last = null;
+        } 
+        // More than one element
+        else {
+            this.first = removeNode.next;
+            removeNode.next = null;
+        }
+
+        this.size--;
+        return removeNode.val;
     }
 }
 
+// Testing the queue
 let queue = new Queue();
 queue.enqueue("Sankalp");
 queue.enqueue("Sahil");
 queue.enqueue("Pratik");
 queue.enqueue("Harsh");
+
+console.log(queue);
+
+console.log(queue.dequeue()); // Sankalp
+console.log(queue.dequeue()); // Sahil
+
 console.log(queue);
