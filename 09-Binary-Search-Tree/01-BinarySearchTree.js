baaa20b9130f66464b40e9cfd6613fe1791dc019
Sankalp <selokarsankalp@gmail.com>
2026-01-04 11:52:34 +0530

    binary search tree setup

---
+++ 09-Binary-Search-Tree/01-BinarySearchTree.js
@@ -0,0 +1,17 @@
+/**
+ *  Create a binary search tree
+ */
+
+class Node{
+    constructor(val){
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinarySearchTree{
+    constructor(){
+        this.root = null;
+    }
+}
