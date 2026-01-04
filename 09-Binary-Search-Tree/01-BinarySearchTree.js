c23e5787d14e7e503cc576b99541ca9d1490a13b
Sankalp <selokarsankalp@gmail.com>
2026-01-04 13:08:55 +0530

    insert()

--- 09-Binary-Search-Tree/01-BinarySearchTree.js
+++ 09-Binary-Search-Tree/01-BinarySearchTree.js
@@ -3,8 +3,8 @@
  */
 
 class Node{
-    constructor(val){
-        this.val = val;
+    constructor(value){
+        this.value = value;
         this.left = null;
         this.right = null;
     }
@@ -14,4 +14,42 @@ class BinarySearchTree{
     constructor(){
         this.root = null;
     }
+    
+    insert(value){
+        let newNode = new Node(value);
+        if(this.root === null){
+            this.root = newNode;
+            return this;
+        }
+        let current = this.root;
+        while(true){
+            if(value === current.value) return undefined;
+            if(value < current.value){
+               if(current.left === null){
+                    current.left = newNode;
+                    return this;
+               }else{
+                    current = current.left;
+               }
+            }else if(value > current.value){
+                if(current.right === null){
+                    current.right = newNode;
+                    return this;
+                }else{
+                    current = current.right;
+                }
+            }
+        }
+    }
 }
+
+let tree = new BinarySearchTree();
+tree.insert(10);
+tree.insert(5);
+tree.insert(13);
+tree.insert(2);
+tree.insert(7);
+tree.insert(11);
+tree.insert(16);
+console.log(tree);
+
